7f2fd24cca58218ea71931204ee9ca936e2f32a8
nathanpoirierisgreat <100322123+nathanpoirierisgreat@users.noreply.github.com>
2022-03-30 11:50:30 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,153 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("welcome to your first class, let's get started.",75)
+basic.showString("the first thing we will learn about is a point. a point is a dot. this is a point.",17)
+basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showString("a point is a point.",75)
+basic.showString("the next thing we will learn about, is what a line is.",78)
+basic.showString("this is a line.")
+    basic.showLeds(`
+        # . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.showString("a line goes on forever.",75)
+basic.showString("next we will learn about a line segment.",75)
+basic.showString("this is a line segment.",75)
+basic.showLeds(`
+        . . . . .
+        . # . . .
+        . . # . .
+        . . . # .
+        . . . . .
+        `)
+    basic.showString("a line segment is part of a line.",75)
+basic.showString("now wee will learn about a ray.",75)
+basic.showString("if you randomly draw a point and then fold the sheet randomly twice so that they cross, and you draw a dot in the center of the x, the fold lines will be the rays.", 75)
+basic.showString("this is an animation explaining what a ray is.",75)
+basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        . # # # #
+        . # . . #
+        . # . . #
+        . # . . #
+        . # # # #
+        `)
+    basic.showLeds(`
+        . . # # #
+        . . # . #
+        . . # . #
+        . . # . #
+        . . # # #
+        `)
+    basic.showLeds(`
+        . # # # #
+        . # . . #
+        . # . . #
+        . # . . #
+        . # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # . # . #
+        # . # . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # . # . #
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # . # . #
+        # # # # #
+        . . . . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # # # # #
+        # . # . #
+        # # # # #
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . # . #
+        # # . # #
+        # . # . #
+        # # # # #
+        `)
+    basic.showString("the lines beside the center point are the rays.",75)
+basic.showString("now we will learn what a vertex is.",75)
+basic.showString("a vertex is where two rays meet.",75)
+basic.showString("what is an angle?")
+    basic.showString("an angle is two lines that meet at the tip of each other.")
+    basic.showString("this is an angle")
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+    basic.showString("this is a 90 degree angle.")
+    basic.showString("to measure an angle, you would use a protractor.")
+    basic.showString("this is a protractor")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # # # .
+        # . . . #
+        # # # # #
+        `)
+    basic.showString("there are 4 types of angle. there is acute.")
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # # # # #
+        `)
+    basic.showString("obtuse")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        . # . . .
+        . . # # #
+        `)
+    basic.showString("straight")
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # # # # #
+        `)
+    basic.showString("and reflex.")
+})
