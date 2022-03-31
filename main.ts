724fc3d0e90b1657d3518387504bff40e2399fba
nathanpoirierisgreat <100322123+nathanpoirierisgreat@users.noreply.github.com>
2022-03-31 13:40:33 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 input.onButtonPressed(Button.A, function () {
     basic.showString("welcome to your first class, let's get started.",75)
-basic.showString("the first thing we will learn about is a point. a point is a dot. this is a point.",17)
+basic.showString("the first thing we will learn about is a point. a point is a dot. this is a point.",75)
 basic.showLeds(`
         . . . . .
         . . . . .
@@ -9,7 +9,7 @@ basic.showLeds(`
         . . . . .
         `)
     basic.showString("a point is a point.",75)
-basic.showString("the next thing we will learn about, is what a line is.",78)
+basic.showString("the next thing we will learn about, is what a line is.",75)
 basic.showString("this is a line.")
     basic.showLeds(`
         # . . . .
@@ -105,49 +105,156 @@ basic.showLeds(`
     basic.showString("the lines beside the center point are the rays.",75)
 basic.showString("now we will learn what a vertex is.",75)
 basic.showString("a vertex is where two rays meet.",75)
-basic.showString("what is an angle?")
-    basic.showString("an angle is two lines that meet at the tip of each other.")
-    basic.showString("this is an angle")
-    basic.showLeds(`
+basic.showString("what is an angle?",75)
+basic.showString("an angle is two lines that meet at the tip of each other.", 75)
+basic.showString("this is an angle", 75)
+basic.showLeds(`
         # . . . .
         # . . . .
         # . . . .
         # . . . .
         # # # # #
         `)
-    basic.showString("this is a 90 degree angle.")
-    basic.showString("to measure an angle, you would use a protractor.")
-    basic.showString("this is a protractor")
-    basic.showLeds(`
+    basic.showString("this is a 90 degree angle.", 75)
+basic.showString("to measure an angle, you would use a protractor.", 75)
+basic.showString("this is a protractor", 75)
+basic.showLeds(`
         . . . . .
         . . . . .
         . # # # .
         # . . . #
         # # # # #
         `)
-    basic.showString("there are 4 types of angle. there is acute.")
-    basic.showLeds(`
+    basic.showString("there are 4 types of angle. there is acute.", 75)
+basic.showLeds(`
         . . . . #
         . . . # .
         . . # . .
         . # . . .
         # # # # #
         `)
-    basic.showString("obtuse")
-    basic.showLeds(`
+    basic.showString("obtuse", 75)
+basic.showLeds(`
         . . . . .
         . . . . .
         # . . . .
         . # . . .
         . . # # #
         `)
-    basic.showString("straight")
-    basic.showLeds(`
+    basic.showString("straight", 75)
+basic.showLeds(`
         # . . . .
         # . . . .
         # . . . .
         # . . . .
         # # # # #
         `)
-    basic.showString("and reflex.")
+    basic.showString("and reflex.", 75)
+basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . . . # .
+        . . . . #
+        `)
+    basic.showString("a reflex angle is when you measure the long way around an angle.", 75)
+basic.showString("what is 360*. ", 75)
+basic.showString("360* is a complete turn.", 75)
+basic.showString("another example is a circle.", 75)
+basic.showString("360* is a complete circle.", 75)
+basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # # .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . # # #
+        . . . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # # #
+        . . . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . # # #
+        . . . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . # .
+        . . . # .
+        . . # # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . . # . #
+        . # # # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . # . . #
+        # # # # .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . . . . #
+        . # . . #
+        # # # . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . # . . #
+        # # # . #
+        . # . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # # # . #
+        . # . . #
+        . # . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showString("that is a 360* turn", 75)
+basic.showString("congratulation, you finished the lesson. if you give us another project, i might get to teach you stuff you know again. until next time", 75)
+basic.showString("this lesson was brought to you by Micromath teacher.", 50)
+basic.showString("creator: NathanPoirier", 50)
+basic.showString("editor: Nathan Poirier", 50)
+basic.showString("animator: Nathan Poirier", 50)
+basic.showString("everything else: Nathan Poirier", 50)
+basic.showString("A Nathan Poirier animation",50)
+basic.showString("all rights reserved",30)
 })
+basic.showString("Hello! i am your micromath teacher, today we will learn about lines, points, angles and mutch mutch more.",75)
+basic.showString("press A to start.")
